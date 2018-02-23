09eedce0a311c996892e88cee3a37b186574c0db
Peem Takoonsawat <mrpeem@gmail.com>
2018-02-23 01:55:02 -0800

    back to a6 files

--- routes/web.js
+++ routes/web.js
@@ -3,10 +3,18 @@ var data = require('../data.json');
 exports.view = function(req, res){
   var categoryTitle = req.params.categoryTitle;
   var itemId = req.params.itemId;
+  var web = req.params.web;
   console.log(categoryTitle);
   console.log(itemId);
-	res.render('share', {
-    'categoryTitle':categoryTitle,
-    'itemId':itemId
+  console.log(req.params);
+
+
+	res.render('web', {
+    'categoryTitle': categoryTitle,
+    'itemId' : itemId,
+    'web' : web
+
+
+
   });
 };
