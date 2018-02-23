adb01c585c195a28964d2987c522f4aaf1abdd72
Peem Takoonsawat <mrpeem@gmail.com>
2018-02-23 03:55:04 -0800

    routes/index

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ var userData = require('../userData.json');
 var popularCategoryList = require('../popularCategoryListData.json');
 var categoryList = require('../categoryListData.json');
 var dataType = require('../dataType.json');
-var data2 = require('../data2.json');
 
 
 exports.view = function(req, res){
