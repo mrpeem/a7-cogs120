890b595ae7fb7f199c82dd3ff14d5fc62f6f0622
Peem Takoonsawat <mrpeem@gmail.com>
2018-02-23 04:25:58 -0800

    app.js

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.get('/profile_register', profile.register);
 // Example route
 // app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
